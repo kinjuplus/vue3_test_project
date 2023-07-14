9ae8b0b0c14a20a3326c3a9eff8f142c71c1cef9
baron.su <baron.su@innolux.com>
2023-07-14 17:11:19 +0800

    enhancement

--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -4,7 +4,7 @@ import tw from '@/lang/tw.json'
 import en from '@/lang/en.json'
 
 const i18n = createI18n({
-  locale: 'tw',           // 設定語言
+  locale:localStorage.getItem("locale") ?? "tw",           // 設定語言
   fallbackLocale: 'tw',   // 若選擇的語言缺少翻譯則退回的語言
   messages: {
     tw,
